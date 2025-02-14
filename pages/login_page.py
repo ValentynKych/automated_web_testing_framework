490a41208e212dc8de0b016027c778375c532b08
Valentyn Kuchynskyi <valentyn_kuch@Valentyns-MacBook-Pro.local>
2025-02-14 13:28:06 +0100

    Refactor page transition: use explicit page initialization in tests

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -8,13 +9,14 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
+        current_url = self.browser.current_url
+        assert "login" in current_url, "URL doesn't contain 'login'"
         assert True
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
         assert True
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not presented"
         assert True
